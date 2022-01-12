774f9d10b63a4ce03a6bf8c6a0eca51197cdc0de
ruthvikr1244 <post.ruthvik@gmail.com>
2022-01-12 21:48:04 +0530

    Commit number 1

---
+++ testLib/src/main/java/com/example/testlib/adder.java
@@ -0,0 +1,8 @@
+package com.example.testlib;
+
+public class adder {
+    public static void add(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println("The sum of the two numbers is " + sum);
+    }
+}
